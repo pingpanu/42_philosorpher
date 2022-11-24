60098dc4b3b2df75f91bc1dbc3dcae2133016f19
Pongpan Ingpanuwong <pingpanu@z3t4c4.42bangkok.com>
2022-11-24 22:04:57 +0700

    edit eat limit exit

--- philo_bonus/main.c
+++ philo_bonus/main.c
@@ -1,12 +1,14 @@
 #include "philo.h"
 
-void    clean_exit(t_env *prog)
+void    clean_exit(t_env *prog, int no_of_philo)
 {
-    kill_all_philos(prog, prog->inputs.no_of_philo);
-    //sem_close(prog->sem_dead);
+    /*in case parent process detect death, it kill process*/
+    if (no_of_philo != 0)
+        kill_all_philos(prog, no_of_philo);
+    /*always called*/
+    sem_close(prog->sem_dead);
     sem_close(prog->sem_forks);
     sem_close(prog->sem_table);
-    //sem_close(prog->sem_write);
     exit(0);
 }
 
@@ -23,9 +25,23 @@ int main(int argc, char **argv)
     philo_init(&prog);
     while (1)
     {
-        waitpid(0, &status, 0);
-        if (WEXITSTATUS(status) == 0 || WEXITSTATUS(status) == 1)
-            clean_exit(&prog);
+        /*if no_of_philo reach zero due to it reach eat limit
+        immediatly stop the program*/
+        if (prog.inputs.no_of_philo == 0)
+                clean_exit(&prog, 0);
+        /*this is parent process to check all child processes (philos)
+        if death detected, kill all child processes, if an philo reach eat
+        limits, kill the philo and decrement no_of_philo*/
+        for (int j = 0 ; j < prog.inputs.no_of_philo; j++) {
+            
+            waitpid(prog.philos[j].philo_pid, &status, 0);
+            if (WEXITSTATUS(status) == 1)
+                clean_exit(&prog, prog.inputs.no_of_philo);
+            if (WEXITSTATUS(status) == 0) {
+                kill(prog.philos[j].philo_pid, SIGKILL);
+                prog.inputs.no_of_philo--;
+            }
+        }
     }
     return (0);
 }
